874324a3dbbc4cd638fada5b7e75010b4405a50d
Bouncy0Knight <114876967+Bouncy0Knight@users.noreply.github.com>
2023-05-16 11:05:56 -0400

    Update Gowers.py

--- Gowers.py
+++ Gowers.py
@@ -7,7 +7,7 @@ import seaborn as sns
 
 import gower
 from kmodes.kprototypes import KPrototypes
-from plotnine import ggplot, geom_line, geom_point, geom_label, labs, xlab, ylab, theme_minimal, aes
+from plotnine import ggplot, geom_line, geom_point, geom_label, labs, xlab, ylab, theme_minimal, aes, theme
 
 # For data visualization
 from sklearn.decomposition import PCA
@@ -20,7 +20,7 @@ from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.linear_model import LinearRegression
 
 # Clustering algorithm
-from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import AgglomerativeClustering, KMeans
 
 # Rand Index
 from sklearn.metrics.cluster import rand_score
@@ -33,9 +33,10 @@ from sklearn.metrics import confusion_matrix
 
 # DATA LOADING
 # //////////////////////////////////////////////////////////////////////////////////////
-# TODO this line just makes each output the same - remove it during the verification proces
+# this line just makes each output the same - useful during the verification process
 np.random.seed(42)
 
+#Read in the location of the file here!
 data_full = df = pd.read_csv('C:/Users/allur/PycharmProjects/pythonProject3/Dartmouth - Courses.csv')
 
 # Drops the first column of data in the courses csv
@@ -51,11 +52,13 @@ data_full.info()  # It looks like we have missing data
 print("\n")
 
 print("MISSING ENTRY INFO")
+#this should quantize the amount of missing data we have
 for i in range(len(data_full.columns)):
     missing_data = data_full[data_full.columns[i]].isna().sum()
     perc = missing_data / len(data_full) * 100
     print('%s,  missing entries: %d, percentage %.2f' % (data_full.columns[i], missing_data, perc))
 print("\n")
+#it looks like we're missing 10% of the data
 
 # HeatMap of missing values to visualize where we have holes in data
 plt.figure(figsize=(6, 5))
@@ -113,9 +116,8 @@ for i, col in enumerate(knn_cols):
 
 plt.tight_layout()
 plt.show()
-
-
 # The distributions match! We can be more confident that our imputations were reasonable
+
 
 # Now we need to consider how to impute the remaining columns (categorical_cols)
 
@@ -124,23 +126,37 @@ plt.show()
 def gower_distance(X, Y=None, w=None):
     return gower.gower_matrix(X, Y, w)
 
-#these are the categorical columns we'll be imputing now
-cat_cols = ['Department', 'Term Number', 'Year']
+# These are the categorical columns we'll be imputing now
+cat_cols = ['Term Number', 'Year', 'Department']
 
-# define the columns to use for imputation
+# Define the columns to use for imputation
 knn_cols = [col for col in data_full_imputed.columns if col not in cat_cols]
 
-# create a copy of the dataframe with only the columns for imputation
+# Create a copy of the dataframe with only the columns for imputation
 data_missing = data_full_imputed[cat_cols].copy()
 
-# impute missing values in the data
-imputer = KNNImputer(n_neighbors=5, weights='distance', metric=gower_distance)
-imputed_data = pd.DataFrame(imputer.fit_transform(data_full_imputed[knn_cols]))
+#To impute categorical variables, we need to transform them somehow to make them manageable
+#I've decided to use one-hot encoding for this purpose, but there are many different options for this step
 
-# combine the imputed data with the original categorical columns
-imputed_data.columns = knn_cols
-imputed_data.index = data_full_imputed.index
-data_imputed = pd.concat([data_full_imputed[cat_cols].fillna('missing'), imputed_data], axis=1)
+# Preprocess categorical columns with one-hot encoding
+encoder = OneHotEncoder()
+encoded_cats = encoder.fit_transform(data_missing)
+encoded_cats_df = pd.DataFrame(encoded_cats.toarray(), columns=encoder.get_feature_names_out(cat_cols))
+
+# Impute missing values in the encoded data
+imputer = KNNImputer(n_neighbors=5, weights='distance', metric=gower_distance)
+imputed_encoded_data = pd.DataFrame(imputer.fit_transform(encoded_cats_df))
+
+# Combine the imputed encoded data with the original categorical columns
+imputed_encoded_data.columns = encoded_cats_df.columns
+imputed_encoded_data.index = data_full_imputed.index
+
+print(data_full_imputed[knn_cols])
+print("_______________________________________")
+print(imputed_encoded_data)
+data_imputed = pd.concat([data_full_imputed[knn_cols], imputed_encoded_data], axis=1)
+print("___________________________")
+print(data_imputed)
 
 #This just plots a heatmap to make sure that all values have been successfully imputed
 plt.figure(figsize=(6, 5))
@@ -151,7 +167,7 @@ plt.show()
 assert data_imputed.isna().sum().sum() == 0
 
 #Success! We've finally finished the imputation step.
-#We can finally start analyzing it
+#We can finally start analyzing the data
 #We'll use an unsupervised method to make predictions
 
 #Lets use K-prototypes to cluster our data first.
@@ -161,59 +177,65 @@ print('Categorical columns           : {}'.format(list(data_imputed.select_dtype
 print('Categorical columns position  : {}'.format(catColumnsPos))
 
 dfMatrix = data_imputed.to_numpy()
-# Choose optimal K using Elbow method
 cost = []
+
+#Elbow method: This clusters the dataset 9 times, using values from 1 to 10 as the number of clusters
+#It'll calculate the associated cost with each cluster and let us know what the optimal number of clusters are
 for cluster in range(1, 10):
-    try:
-        kprototype = KPrototypes(n_jobs = -1, n_clusters = cluster, init = 'Huang', random_state = 0)
-        kprototype.fit_predict(dfMatrix, categorical = catColumnsPos)
-        cost.append(kprototype.cost_)
-        print('Cluster initiation: {}'.format(cluster))
-    except:
-        break
+    kmean = KMeans(n_clusters=cluster, init='random', random_state=0, n_init = 10)
+    kmean.fit(dfMatrix)
+    cost.append(kmean.inertia_)
+    print('K-means cost is: ' + str(kmean.inertia_))
+    print('Cluster initiation: {}'.format(cluster))
+
 # Converting the results into a dataframe and plotting them
-df_cost = pd.DataFrame({'Cluster':range(1, 6), 'Cost':cost})
+print(cost)
+df_cost = pd.DataFrame({'Cluster': range(1, 10), 'Cost': cost})
+
 # Data viz
-plotnine.options.figure_size = (8, 4.8)
-(
-    ggplot(data = df_cost)+
-    geom_line(aes(x = 'Cluster',
-                  y = 'Cost'))+
-    geom_point(aes(x = 'Cluster',
-                   y = 'Cost'))+
-    geom_label(aes(x = 'Cluster',
-                   y = 'Cost',
-                   label = 'Cluster'),
-               size = 10,
-               nudge_y = 1000) +
-    labs(title = 'Optimal number of cluster with Elbow Method')+
-    xlab('Number of Clusters k')+
-    ylab('Cost')+
-    theme_minimal()
+plot = (
+    ggplot(data=df_cost) +
+    geom_line(aes(x='Cluster', y='Cost')) +
+    geom_point(aes(x='Cluster', y='Cost')) +
+    geom_label(aes(x='Cluster', y='Cost', label='Cluster'), size=10, nudge_y=1000) +
+    labs(title='Optimal number of clusters with Elbow Method') +
+    xlab('Number of Clusters k') +
+    ylab('Cost') +
+    theme_minimal() +
+    theme(figure_size=(8, 4.8))
 )
+print(plot)
+
+#Great! By the elbow method, we can graphically see that the optimal number of clusters is 4. (Note, the elbow method
+#can be graphically seen as the inflection point).
+
+#Clustering via Kmeans, using 4 as the value for number of clusters
+k = 4
+kmean = KMeans(n_clusters=k, init='random', random_state=0, n_init=10)
+kmean.fit(dfMatrix)
+cluster_labels = kmean.labels_
+cluster_centroids = kmean.cluster_centers_
+
+#This plots the clusters we've created.
+def plotCentroids(feature1, feature2, features_list):
+    feature_1_ind = features_list.index(feature1)  # Replace with the index of Feature A
+    feature_2_ind = features_list.index(feature2)  # Replace with the index of Feature B
+
+    # Plot the cluster centroids
+    plt.scatter(cluster_centroids[:, feature_1_ind], cluster_centroids[:, feature_2_ind], c='red', marker='x',
+                s=100)
+    plt.xlabel(feature_names[feature_1_ind])
+    plt.ylabel(feature_names[feature_2_ind])
+    plt.title('Cluster Centroids')
+    plt.show()
+
+#The different types of available features
+feature_names = data_imputed.columns
+print(feature_names)
+feature_names = feature_names.tolist()
 
 
-
-
-# # First, define the number of clusters
-# n_clusters = 5
-#
-# # re-define the categorical columns
-# cat_cols = ['Department', 'Term Number', 'Year']
-#
-# # define the numeric columns
-# num_cols = ['Number of Sections', 'Enrollments', 'Median GPA Points', 'Average Section Size', 'Course Number']
-#
-# # combine the categorical and numeric data into one array
-# data_array = np.column_stack((data_imputed[cat_cols], data_imputed[num_cols]))
-#
-# # define the k-prototypes model
-# kproto = KPrototypes(n_clusters=n_clusters, init='Cao', verbose=2)
-#
-# # fit the model to the data
-# clusters = kproto.fit_predict(data_array, categorical=list(range(len(cat_cols))))
-#
-# # add the cluster labels to the original data frame
-# data_imputed['cluster'] = clusters
-#
-#
+plotCentroids('Median GPA Points', 'Department_ECON', feature_names) #The model predicts that As the median GPA decreases, the chance that a class is an ECON class increases
+plotCentroids('Average Section Size', 'Median GPA Points', feature_names) #The model predicts that as the average section size decreases, the GPA of the class increases
+plotCentroids('Number of Sections', 'Average Section Size', feature_names) #The model predicts that as the number of sections increases, the average section size will increase
+plotCentroids('Course Number', 'Median GPA Points', feature_names) #The model predicts that low and high course numbers will have higher GPAs than intermediate course numbers
